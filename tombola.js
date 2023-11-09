4fb4f8f0a56a8c6e72a5b06e003c8ac9ff38b728
MatteoRomano7 <148786683+MatteoRomano7@users.noreply.github.com>
2023-11-09 17:02:33 +0100

    Add files via upload

---
+++ tombola.js
@@ -0,0 +1,39 @@
+const tombolaArray = Array.from({ length: 90 }, (_, i) => i + 1);
+
+const creaTabellone = function () {
+  const tabelloneContainer = document.getElementById("tabellone");
+
+  for (let i = 1; i <= 90; i++) {
+    const containerNumeri = document.createElement("div");
+    const cellaNumeri = document.createElement("h3");
+    cellaNumeri.textContent = i;
+    containerNumeri.appendChild(cellaNumeri);
+    tabelloneContainer.appendChild(containerNumeri);
+  }
+};
+
+function estraiNumeroRandom() {
+  if (tombolaArray.length === 0) {
+    alert("Hai estratto tutti i numeri!");
+    return;
+  }
+  const numeroRandom = Math.floor(Math.random() * tombolaArray.length);
+  const numeroEstratto = tombolaArray[numeroRandom];
+  tombolaArray.splice(numeroRandom, 1);
+  return numeroEstratto;
+}
+
+function cliccaEstrai() {
+  const numeroEstratto = estraiNumeroRandom();
+  document.getElementById("estrazione").textContent =
+    "Numero estratto: " + numeroEstratto;
+  const tombola = document.getElementById("tabellone");
+  const selezionaCella = tombola.querySelector(
+    " h3:contains('" + numeroEstratto + "')"
+  ).parentElement;
+  selezionaCella.classList.add("estratto");
+}
+
+creaTabellone();
+
+document.getElementById("estrai").addEventListener("click", cliccaEstrai);
